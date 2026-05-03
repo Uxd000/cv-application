d6ae0d763ce8e91ed11d03f88c1c40935cd62064
Usaid Ahmed <10usaidahmed@gmail.com>
2026-05-03 08:33:52 +0530

    changed preview to Preview

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import GeneralInfo from "./components/GeneralInfo";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
-import Preview from "./components/preview";
+import Preview from "./components/Preview";
 import "./styles/app.css";
 
 function App() {
